a54f162a1433b73cc47b9f6d29667635f28f3924
Samuel Janas <sneakyinsect@gmail.com>
2023-08-21 08:53:49 +0200

    dataset revert

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -16,16 +16,16 @@ def create_embeddings(cfg, autoencoder, data_loader):
     :param autoencoder: autoencoder instance
     :param data_loader: data loader
     """
-    autoencoder.eval()
-    autoencoder.to(autoencoder.device)
+    autoencoder_instance.eval()
+    autoencoder_instance.to(autoencoder_instance.device)
     embeddings = []
     labels = []
 
     for batch_idx, batch in enumerate(data_loader):
-        data = batch["image"].float().to(autoencoder.device) / 255.0
-        target = batch["label"].to(autoencoder.device)
+        data = batch["image"].float().to(autoencoder_instance.device) / 255.0
+        target = batch["label"].to(autoencoder_instance.device)
 
-        embedding = autoencoder.encode(data)
+        embedding = autoencoder_instance.encode(data)
         embeddings.append(embedding.cpu().numpy())
         labels.append(target.cpu().numpy())
 
@@ -36,36 +36,20 @@ def create_embeddings(cfg, autoencoder, data_loader):
 
 
 if __name__ == "__main__":
-    checkpoint_path_list = [
-        "checkpoints/AE_MNIST_checkpoint_epoch_10_embSize_32.pt",
-        "checkpoints/AE_MNIST_checkpoint_epoch_10_embSize_16.pt",
-        "checkpoints/AE_MNIST_checkpoint_epoch_10_embSize_8.pt",
-    ]
-    test_embeddings_list = []
-    test_labels_list = []
-    train_embeddings_list = []
-    train_labels_list = []
+    checkpoint_path = "checkpoints/AE_MNIST_checkpoint_epoch_10_embSize_32.pt"
+    checkpoint = torch.load(checkpoint_path)
 
-    # make sure the loaders are the same
-    checkpoint = torch.load(checkpoint_path_list[0])
     train_loader, test_loader, input_size = get_data_loaders(checkpoint["config"], return_targets=True)
 
-    for checkpoint_path in checkpoint_path_list:
-        if checkpoint_path != checkpoint_path_list[0]:
-            checkpoint = torch.load(checkpoint_path)
+    autoencoder_instance = initialize_model(checkpoint["config"], input_size)
+    autoencoder_instance.load_state_dict(checkpoint["model_state_dict"])
 
-        autoencoder_instance = initialize_model(checkpoint["config"], input_size)
-        autoencoder_instance.load_state_dict(checkpoint["model_state_dict"])
+    optimizer = torch.optim.Adam(autoencoder_instance.parameters(), checkpoint["config"].train.lr)
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
-        optimizer = torch.optim.Adam(autoencoder_instance.parameters(), checkpoint["config"].train.lr)
-        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    test_embeddings, test_labels = create_embeddings(checkpoint["config"], autoencoder_instance, test_loader)
 
-        test_embeddings, test_labels = create_embeddings(checkpoint["config"], autoencoder_instance, test_loader)
-        test_embeddings_list.append(test_embeddings)
-        test_labels_list.append(test_labels)
-        train_embeddings, train_labels = create_embeddings(checkpoint["config"], autoencoder_instance, train_loader)
-        train_embeddings_list.append(train_embeddings)
-        train_labels_list.append(train_labels)
+    train_embeddings, train_labels = create_embeddings(checkpoint["config"], autoencoder_instance, train_loader)
 
     DATASET_NAME = "SneakyInsect/MNIST-preprocessed"
     HUGGINGFACE_TOKEN = os.environ["HUGGINGFACE_TOKEN"]
@@ -73,22 +57,16 @@ if __name__ == "__main__":
     test_data = {
         "image": test_loader.dataset["image"].numpy(),
         "label": test_labels,
-        "embedding32": test_embeddings_list[0],
-        "embedding16": test_embeddings_list[1],
-        "embedding8": test_embeddings_list[2],
+        "embedding": test_embeddings,
     }
     train_data = {
         "image": train_loader.dataset["image"].numpy(),
         "label": train_labels,
-        "embedding32": train_embeddings_list[0],
-        "embedding16": train_embeddings_list[1],
-        "embedding8": train_embeddings_list[2],
+        "embedding": train_embeddings,
     }
-    # print(test_data["image"].shape, test_data["label"].shape, test_data["embedding"].shape)
-    # print(train_data["image"].shape, train_data["label"].shape, train_data["embedding"].shape)
+
     test_dataset = Dataset.from_dict(test_data)
     train_dataset = Dataset.from_dict(train_data)
 
     dataset_dict = DatasetDict({"train": train_dataset, "test": test_dataset})
-    # dataset_dict.save_to_disk(DATASET_NAME)
     dataset_dict.push_to_hub(DATASET_NAME, token=HUGGINGFACE_TOKEN)
--- train_random_tree.py
+++ train_random_tree.py
@@ -8,16 +8,10 @@ def load_preprocessed_dataset(dataset_name):
     Loads the preprocessed dataset from the HuggingFace Hub.
     """
     data = load_dataset(dataset_name)
-    embedding_sizes = [32, 16, 8]
-    train_embeddings = []
-    train_labels = []
-    test_embeddings = []
-    test_labels = []
-    for embedding_size in embedding_sizes:
-        train_embeddings.append(data["train"][f"embedding{embedding_size}"])
-        train_labels.append(data["train"]["label"])
-        test_embeddings.append(data["test"][f"embedding{embedding_size}"])
-        test_labels.append(data["test"]["label"])
+    train_embeddings = data["train"]["embedding"]
+    train_labels = data["train"]["label"]
+    test_embeddings = data["test"]["embedding"]
+    test_labels = data["test"]["label"]
 
     return train_embeddings, train_labels, test_embeddings, test_labels
 
@@ -25,11 +19,10 @@ def load_preprocessed_dataset(dataset_name):
 if __name__ == "__main__":
     train_embeddings, train_labels, test_embeddings, test_labels = load_preprocessed_dataset("SneakyInsect/MNIST-preprocessed")
 
-    for i in range(3):
-        clf = RandomForestClassifier(n_estimators=100)
-        clf.fit(train_embeddings[i], train_labels[i])
-        # Predict the labels of the test data
-        predictions = clf.predict(test_embeddings[i])
-        # Calculate the accuracy of the predictions
-        accuracy = accuracy_score(test_labels[i], predictions)
-        print(f"Accuracy of the Random Forest classifier with embedding size {2**(5-i)}: {accuracy*100:.2f}%")
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(train_embeddings, train_labels)
+    # Predict the labels of the test data
+    predictions = clf.predict(test_embeddings)
+    # Calculate the accuracy of the predictions
+    accuracy = accuracy_score(test_labels, predictions)
+    print(f"Accuracy of the Random Forest classifier with embedding size {32}: {accuracy*100:.2f}%")
