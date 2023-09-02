485386bff529a75ab79ece83ce3b05962c6721f2
Tomasz Roszczynialski <roszcz@gmail.com>
2023-09-02 12:53:33 +0200

    vq cleanup

--- models/VQVAE.py
+++ models/VQVAE.py
@@ -36,11 +36,11 @@ class ResidualStack(nn.Module):
 class Encoder(nn.Module):
     def __init__(
         self,
-        in_channels,
-        num_hiddens,
-        num_downsampling_layers,
-        num_residual_layers,
-        num_residual_hiddens,
+        in_channels: int,
+        num_hiddens: int,
+        num_downsampling_layers: int,
+        num_residual_layers: int,
+        num_residual_hiddens: int,
     ):
         super(Encoder, self).__init__()
 
@@ -79,11 +79,11 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(
         self,
-        embedding_dim,
-        num_hiddens,
-        num_upsampling_layers,
-        num_residual_layers,
-        num_residual_hiddens,
+        embedding_dim: int,
+        num_hiddens: int,
+        num_upsampling_layers: int,
+        num_residual_layers: int,
+        num_residual_hiddens: int,
     ):
         super(Decoder, self).__init__()
 
@@ -130,27 +130,13 @@ class VQVAE(nn.Module):
         super().__init__()
 
         self.cfg = cfg
-        in_channels = cfg.in_channels
-        num_hiddens = cfg.num_hiddens
-
-        num_upsampling_layers = cfg.num_upsampling_layers
-        num_downsampling_layers = cfg.num_downsampling_layers
-        num_residual_layers = cfg.num_residual_layers
-        num_residual_hiddens = cfg.num_residual_hiddens
-
-        embedding_dim = cfg.embedding_dim
-        num_embeddings = cfg.num_embeddings
-
-        use_ema = cfg.use_ema
-        decay = cfg.decay
-        epsilon = cfg.epsilon
 
         self.encoder_layer = Encoder(
-            in_channels=in_channels,
-            num_hiddens=num_hiddens,
-            num_downsampling_layers=num_downsampling_layers,
-            num_residual_layers=num_residual_layers,
-            num_residual_hiddens=num_residual_hiddens,
+            in_channels=cfg.in_channels,
+            num_hiddens=cfg.num_hiddens,
+            num_downsampling_layers=cfg.num_downsampling_layers,
+            num_residual_layers=cfg.num_residual_layers,
+            num_residual_hiddens=cfg.num_residual_hiddens,
         )
         self.pre_vq_conv_layer = nn.Conv2d(
             in_channels=cfg.num_hiddens,
@@ -158,18 +144,18 @@ class VQVAE(nn.Module):
             kernel_size=1,
         )
         self.vector_quantizer = VectorQuantizer(
-            embedding_dim=embedding_dim,
-            num_embeddings=num_embeddings,
-            use_ema=use_ema,
-            decay=decay,
-            epsilon=epsilon,
+            embedding_dim=cfg.embedding_dim,
+            num_embeddings=cfg.num_embeddings,
+            use_ema=cfg.use_ema,
+            decay=cfg.decay,
+            epsilon=cfg.epsilon,
         )
         self.decoder_layer = Decoder(
-            embedding_dim=embedding_dim,
-            num_hiddens=num_hiddens,
-            num_upsampling_layers=num_upsampling_layers,
-            num_residual_layers=num_residual_layers,
-            num_residual_hiddens=num_residual_hiddens,
+            embedding_dim=cfg.embedding_dim,
+            num_hiddens=cfg.num_hiddens,
+            num_upsampling_layers=cfg.num_upsampling_layers,
+            num_residual_layers=cfg.num_residual_layers,
+            num_residual_hiddens=cfg.num_residual_hiddens,
         )
 
     def forward(self, x):
